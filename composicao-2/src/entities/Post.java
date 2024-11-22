cfa5dfc26cb5d658d8b3e61c1fc4d5a693961957
Caio Marquez Chaves <caio.chaves@oobj.com.br>
2024-11-22 09:58:43 -0300

    curso-estudo

---
+++ composicao-2/src/entities/Post.java
@@ -0,0 +1,57 @@
+package entities;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Post {
+
+    private Date moment;
+    private String title;
+    private String content;
+    private Integer likes;
+
+
+    private List<Comment> comments = new ArrayList<>();
+
+    public Post(){
+    }
+    public Post(Date moment, String title, String content, Integer likes) {
+        this.moment = moment;
+        this.title = title;
+        this.content = content;
+        this.likes = likes;
+    }
+
+    public Date getMoment() {
+        return moment;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public Integer getLikes() {
+        return likes;
+    }
+
+    public void setMoment(Date moment) {
+        this.moment = moment;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setLikes(Integer likes) {
+        this.likes = likes;
+    }
+}
---
+++ composicao/src/enums/WorkLevel.java
@@ -0,0 +1,8 @@
+package enums;
+
+public enum WorkLevel {
+
+    JUNIOR,
+    MID_LEVEL,
+    SENHIOR,
+}

08a875c6738a0f7836acc4c33e48a7175e6e759c
Caio Marquez Chaves <caio.chaves@oobj.com.br>
2025-01-30 17:35:10 -0300

    curso-estudo

--- abstract-methed/src/application/Program.java
+++ abstract-methed/src/application/Program.java
@@ -1,5 +1,12 @@
 package application;
 
+import entities.Circle;
+import entities.Color;
+import entities.Rectangle;
+import entities.Shape;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -11,9 +18,33 @@ public class Program {
         System.out.println("Enter the number of shapes");
         int n = sc.nextInt();
 
+        List <Shape> list = new ArrayList<>();
+
         for (int i = 1; i < n; i++) {
-            System.out.println("Shape #" + i + " data:");
+            System.out.print("Shape #" + i + " data:");
+            System.out.print("Rectangle or Circle (r/c)?");
+            char ch = sc.next().charAt(0);
+            System.out.print("Color (BLACK/BLUE/RED)");
+            Color color = Color.valueOf(sc.next());
+            if ( ch == 'r'){
+                System.out.print("Width: ");
+                double width = sc.nextDouble();
+                System.out.print("Height: ");
+                double height = sc.nextDouble();
+                list.add(new Rectangle(color, width, height));
+            }
+            else {
+                System.out.println("Radius: ");
+                double radius = sc.nextDouble();
+                list.add(new Circle(color, radius));
+            }
+
             System.out.println();
+            System.out.println("Shape areas:");
+            for (Shape shape : list){
+                System.out.println(shape.area());
+                System.out.println(String.format("%.2f", shape.area()));
+            }
 
         }
     }

0ec3e70e357d03eaecdd1393d924caeecc69c8c6
Caio Marquez Chaves <caiomchaves@gmail.com>
2025-02-12 19:33:59 -0300

    class 161 - revision

--- class-161-exerc/src/application/Program.java
+++ class-161-exerc/src/application/Program.java
@@ -1,6 +1,10 @@
 package application;
 
+import entities.Product;
+
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Program {
@@ -11,30 +15,38 @@ public class Program {
         System.out.println("Enter the name of folder path: ");
         String strPath = sc.nextLine();
 
-        File path = new File(strPath);
-        File output = new File("C:\\Users\\Caio\\Desktop\\arquivo2.csv");
+        List<Product> list = new ArrayList<Product>();
 
-        try (BufferedReader br = new BufferedReader(new FileReader(path));
-        BufferedWriter bw = new BufferedWriter(new FileWriter(output))) {
+        File path = new File(strPath);
+
+        boolean sucess = new File(strPath + "\\out").mkdir();
+
+        File output = new File(strPath + "\\out\\summary.csv");
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
             
             String line = br.readLine();
             while (line != null) {
-                
-              String[] parts = line.split(",");
-               if (parts.length == 3) {
-                   String name = parts[0];
-                   Double price = Double.parseDouble(parts[1]);
-                   Integer amount = Integer.parseInt(parts[2]);
-                   
-                   double result = price * amount;
 
-                   bw.write(name + "," + result);
-                   bw.newLine();
-               }
-              
-              line = br.readLine();
+                String[] parts = line.split(",");
+                String name = parts[0];
+                Double price = Double.parseDouble(parts[1]);
+                Integer quanttity = Integer.parseInt(parts[2]);
+
+                list.add(new Product(name, price, quanttity));
+                line = br.readLine();
+
             }
-            System.out.println("Arquivo 2 criado com sucesso!");
+            try (BufferedWriter bw = new BufferedWriter(new FileWriter(output))) {
+
+                for (Product p : list) {
+                    bw.write(p.getName() + "," + p.getPrice() * p.getQuantity());
+                    bw.newLine();
+                }
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+
 
         }catch (IOException e){
             System.out.println("Error: " + e.getMessage());
